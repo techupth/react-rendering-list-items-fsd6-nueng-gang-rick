d463048c12f6cad075e74ebc76ecee3943c7f604
Wascharapong Phummon <rickyharvey.w@gmail.com>
2024-06-05 00:27:28 +0700

    feat: movie list

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,16 @@
+
+export function MoviesList(props) {
+  return (
+    <div className="movie-box">
+      <img src={props.image} alt="banner-movie" />
+      <article>
+        <span>Title: {props.title}</span>
+        <span>Year: {props.year}</span>
+        <span>Runtime: {props.runtime}</span>
+        <span>Genres: {props.genres}</span>
+        <span>IMDB Rating: {props.imdbRating}</span>
+        <span>IMDB Votes: {props.imdbVoting}</span>
+      </article>
+    </div>
+  );
+}
